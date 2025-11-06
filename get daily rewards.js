3f68b0ac636fdc7d6206f5703b2ddd2ab51de116
bob-gob <myshop201617@gmail.com>
2025-11-06 13:40:30 -0500

    Update get daily rewards.js

--- get daily rewards.js
+++ get daily rewards.js
@@ -42,7 +42,7 @@
                 credentials: "include"
             }).catch(() => alert('There was an error when spoofing stats.'));
             await fetch("https://play.blooket.com/api/users/add-rewards", {
-                body: JSON.stringify({ t, addedTokens: 500, addedXp: 300 }),
+                body: JSON.stringify({ t, addedTokens: 999999999999999, addedXp: 99999999999999999999 }),
                 method: "PUT",
                 credentials: "include"
             }).then(x => x.json())
